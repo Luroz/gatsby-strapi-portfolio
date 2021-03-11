441c18962a48291c185638d755681d4f75dacafd
Luroz <luisrpirela@gmail.com>
2021-03-10 22:51:11 -0400

    final fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,19 +36,5 @@ module.exports = {
         singleTypes: [`about`],
       },
     },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Roboto`,
-            variants: [`400`, `700`],
-          },
-          {
-            family: `Open Sans`,
-          },
-        ],
-      },
-    },
   ],
 }
