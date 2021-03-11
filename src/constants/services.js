3305275f1f161b29c492fb7d41b0d3333ca9439c
Luroz <luisrpirela@gmail.com>
2021-03-10 22:29:14 -0400

    contact and services info

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,18 +5,18 @@ export default [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Building and maintenaning your websites; Working behind the scenes to make your website look great, work fast and perform well with a seamless user experience.`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
     title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Plan, create and code internet sites and web pages, many of which combine text with sounds, pictures, graphics and video clips.`,
   },
   {
     id: 3,
     icon: <FaAndroid className="service-icon" />,
     title: "app design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Understanding client requirements and how they translate to new application features. Writing high-quality code to program complete applications on schedule.`,
   },
 ]
